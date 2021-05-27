77f60e4ab48e7382ad59f53276db95de14b7a4ab
Jonatas de Oliveira Coelho <jonatas.oliveiracoelho@gmail.com>
2021-05-26 23:33:38 -0300

    Finish Search Results component

---
+++ src/components/SearchResults/SearchResults.tsx
@@ -0,0 +1,89 @@
+/* eslint-disable camelcase */
+import React, { useCallback, useState } from 'react';
+import InfiniteScroll from 'react-infinite-scroll-component';
+import api from '../../services/api';
+import { LoadingIcon } from '../Loading/Loading.styles';
+
+import RepositoryCard from '../RepositoryCard';
+
+import { Container } from './SearchResults.styles';
+
+interface RepositoryData {
+  id: string;
+  name: string;
+  full_name: string;
+  owner: { avatar_url: string; login: string };
+  description: string;
+}
+
+interface SearchResultsProps {
+  results: RepositoryData[];
+  query: string;
+  setResults: (repositoryData: RepositoryData[]) => void;
+  totalCount: number;
+}
+
+const SearchResults: React.FC<SearchResultsProps> = ({
+  results,
+  totalCount,
+  query,
+  setResults,
+}) => {
+  const [hasMore, setHasMore] = useState(true);
+  const [page, setPage] = useState(2);
+
+  const handleNext = useCallback(async () => {
+    if (results.length === totalCount) {
+      setHasMore(false);
+      return;
+    }
+
+    const response = await api.get(`/search/repositories`, {
+      params: {
+        q: encodeURIComponent(query),
+        sort: 'stars',
+        order: 'desc',
+        per_page: 15,
+        page: page + 1,
+      },
+    });
+
+    const newResults = response.data.items;
+
+    setResults([...results, ...newResults]);
+
+    setPage(page + 1);
+  }, [page, query, results, setResults, totalCount]);
+
+  return (
+    <Container>
+      <InfiniteScroll
+        dataLength={results.length}
+        next={handleNext}
+        hasMore={hasMore}
+        loader={
+          <div>
+            <LoadingIcon /> Loading news Repositories...
+          </div>
+        }
+        endMessage={
+          <p style={{ textAlign: 'center' }}>
+            <b>No more Repositories to show</b>
+          </p>
+        }
+      >
+        {results.map(repository => (
+          <RepositoryCard key={repository.id} repository={repository} />
+        ))}
+
+        {results.length < 16 && (
+          <p style={{ textAlign: 'center' }}>
+            <b>No more Repositories to show</b>
+          </p>
+        )}
+      </InfiniteScroll>
+    </Container>
+  );
+};
+
+export default SearchResults;
